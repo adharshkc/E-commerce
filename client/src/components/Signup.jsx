4f5e7befd34334a6ef863f5b02e3be474e3d3284
adharshkc <adharshkc2017@gmail.com>
2023-09-08 22:16:02 +0530

    added server and client

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,161 @@
+import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineEye } from "react-icons/ai";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+
+const Signup = () => {
+  const [firstname, setFirtname] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [conPassword, setConPassword] = useState('')
+  const [visible, setVisible] = useState(false);
+  const [visible1, setVisible1] = useState(false);
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    alert(` welcome ${firstname}`)
+  }
+  return (
+    <div className="min-h-screen bg-gray-100 py-4 flex flex-col justify-center sm:py-10">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-9 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              <h1 className="text-2xl text-gray-800 font-semibold text-center">
+                Create Account
+              </h1>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <form onSubmit={handleSubmit} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="firstname"
+                    value={firstname}
+                    name="firstname"
+                    type="text"
+                    required
+                    onChange={(e) => setFirtname(e.target.value)}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Email address"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    First Name
+                  </label>
+                </div>
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="email"
+                    value={email}
+                    name="email"
+                    required
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Email address"
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Email Address
+                  </label>
+                </div>
+                
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    required
+                    type={visible1 ? "text" : "password"}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Password"
+                  />
+
+                  <label
+                    htmlFor="password"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Password
+                  </label>
+                  {visible1 ? (
+                    <AiOutlineEye
+                      className="absolute right-2 top-2 cursor-pointer"
+                      size={25}
+                      onClick={() => setVisible1(false)}
+                    />
+                  ) : (
+                    <AiOutlineEyeInvisible
+                      className="absolute right-2 top-2 cursor-pointer"
+                      size={25}
+                      onClick={() => setVisible1(true)}
+                    />
+                  )}
+                </div>
+                <div className="relative">
+                  <input
+                    autoComplete="off"
+                    id="conPassword"
+                    value={conPassword}
+                    onChange={(e) => setConPassword(e.target.value)}
+                    name="conPassword"
+                    required
+                    type={visible ? "text" : "password"}
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Password"
+                  />
+
+                  <label
+                    htmlFor="password"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Confirm Password
+                  </label>
+                  {visible ? (
+                    <AiOutlineEye
+                      className="absolute right-2 top-2 cursor-pointer "
+                      size={25}
+                      onClick={() => setVisible(false)}
+                    />
+                  ) : (
+                    <AiOutlineEyeInvisible
+                      className="absolute right-2 top-2 cursor-pointer"
+                      size={25}
+                      onClick={() => setVisible(true)}
+                    />
+                  )}
+                </div>
+                <div className="relative">
+                  <button className="bg-blue-500 text-white rounded-md px-2 py-1">
+                    Submit
+                  </button>
+                </div>
+                {/* <div className="flex items-center">
+                  <input type="checkbox" />
+                  <h4 className="ml-1 text-base ">Remember me</h4>
+                </div> */}
+              </form>
+
+              <div className="text-base  text-gray-700 flex items-center">
+                <h4>Existing User?</h4>
+                <Link to={"/login"} className="text-blue-600  hover:text-blue-500 pl-2">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
